2205252be2768db742523f947fd13b577e0602c9
andeeisaacs <andeefonder@gmail.com>
2021-03-18 11:41:20 -0700

    Add more descriptive comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Madstory from './Components/Madstory/Madstory'
 
 function App() {
   //useState hook to read and set the state of Inputs. useState replaces our lifecycle methods, Mount, Update and Unmount. Uses a blank string as an initalized value for our state.
   const [inputs, setInputs] = useState("")
 
-  // useState to read and set the state of the story to a boolean value. False is used as our initial value to our state,  keeping the story component from rendering until set to true by the handleSubmit function call later. 
+  // One more useState to read and set the state of the story to a boolean value. False is used as our initial value to our state,  keeping the story component from rendering until set to true by the handleSubmit function call later. 
   const [storyComplete, setStoryComplete] = useState(false)
   
-  // handleSubmit is called by our form's submit button, keeps the page from refreshing and accesses the useState hook for storyComplete to change it to true. Allows the conditional rendering to allow our story to appear.
+  // handleSubmit is called by our form's submit button, keeps the page from refreshing and accesses the useState setter hook for storyComplete to change it to true. Allows the conditional rendering to allow our story to appear.
   const handleSubmit = (e) => {
     e.preventDefault()
     setStoryComplete(true)
   }
 
   //refreshPage is called onclick by our clear button in our form, allowing the user to refresh the page. Can be refactored to something more efficient such as a cleanup to reset our states back to default values. 
+  //The .reload is a depreciated bit of code, but it still runs and is used right now to simply complete the project. 
   const refreshPage = () => {
     window.location.reload(false);
   }
+
+  // This is.. quite useless to be honest. HOWEVER, it was practice to see how useEffect works. It updates the title of the page with what ever is the current value of the nouneOne state value, as written in the first noun input box.  Its neat, but nearly a direct example usage from the React hooks documentation on useEffect. 
+  useEffect(() => {
+    document.title = `Your first noun is ${inputs.nounOne}`
+  });
 
   return (
       <>
@@ -29,6 +35,7 @@ function App() {
               placeholder="Type a name."
               type="text"
               value={inputs.nounOne}
+              // Neat stuff jere- we bind target, which is the input box here, to our setter we defined earlier on with useState, we then say take a look into our object and if the key:value doesnt exist, create the key and slap our value of input into it every time the input box is changed. In the end, this is nice because we do not need to create a ton of variables early on for each state. Input for our read, setInput for our setter. I am sure there is more elegant ways of doing this, and more of a 'DRY' way, but for right now, this works well. 
               onChange={({target}) => setInputs(state => ({...state, nounOne:target.value}))}
               />
 
@@ -63,7 +70,7 @@ function App() {
               <input 
               key="adjOne"
               type="text"
-              placeholder="Type a descriptive adjective."
+              placeholder="Type a adjective."
               value={inputs.adjOne}
               onChange={({target}) => setInputs(state => ({...state,adjOne:target.value}))}
               />
@@ -72,7 +79,7 @@ function App() {
               <input 
               key="verbTwo"
               type="text"
-              placeholder="Type a DANGEROUS action!"
+              placeholder="Type an action verb!"
               value={inputs.verbTwo}
               onChange={({target}) => setInputs(state => ({...state,verbTwo:target.value}))}
               />
@@ -81,7 +88,7 @@ function App() {
               <input 
               key="adjTwo"
               type="text"
-              placeholder="Type a SUPER DANGEROUS adjective."
+              placeholder="Type an adjective."
               value={inputs.adjTwo}
               onChange={({target}) => setInputs(state => ({...state,adjTwo:target.value}))}
               />
@@ -103,6 +110,8 @@ function App() {
               <button onClick={refreshPage}>Clear</button>
           </form>
           <hr />
+
+          {/* Quick conditional render here so that our submit button is not useless. The form will only render if the submit button has been clicked. It does not check, however, if the fields have been filled out at all. */}
           {storyComplete ? <Madstory 
         nounOne = {inputs.nounOne}
         nounTwo = {inputs.nounTwo}
--- src/Components/Madstory/Madstory.js
+++ src/Components/Madstory/Madstory.js
@@ -1,4 +1,5 @@
 
+// Fair warning here, I did NOT write this story to utilize the 'correct' parts of english. I simply slapped the story together for sake of getting the props passed down and to demonstrate utilization of the same props again and again. 
 
 function Madstory({nounOne, nounTwo, nounThree, nounFour, verbOne, verbTwo, adjOne, adjTwo}) {
     return (
